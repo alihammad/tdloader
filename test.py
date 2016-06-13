5c13b83fb050e9a19d64f4696d61b0344283bdbc
Baig <ali.hammadbaig@gmail.com>
2016-06-13 22:05:14 +1000

    experimenting with show table statement

--- test.py
+++ test.py
@@ -8,9 +8,26 @@ class DB():
 
 	def fun1(self):
 		# session.execute("create table financial.dummytable1(a varchar(10))")
-		rows = self.session.execute("SHOW TABLE financial.dummytable1").fetchone()[0]
-		for row in rows:
-			print(row)
+		cur = self.session.cursor()
+		cur.arraysize = 5000
+		# rows = cur.execute("SHOW TABLE financial.dummytable1")
+		# for row in rows:
+		# 	print(row)
+
+		# cur.close()
+
+		# for row in self.session.execute("SHOW TABLE financial.dummytable1"):
+		# 	print(row)
+
+		cur.execute("show table financial.dummytable1")
+		# count = cur.fetchall()[0][0]
+		# print("Count: {0} ".format(count))
+
+		stmt = cur.fetchall()
+		print(len(stmt))
+			
+		cur.close()
+
 db = DB()
 db.fun1()
 
